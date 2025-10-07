c8e3d44d973ab8d96cbc701f2c5ada0ce22bb394
olivier-bonnin <olivier.bonnin@laplateforme.io>
2025-10-07 07:09:40 +0200

    chore: ignore checkpoints, caches; tidy repo

--- olivier-maj/scripts/.ipynb_checkpoints/boruta_analysis-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from boruta import BorutaPy
-from sklearn.ensemble import RandomForestClassifier
-
-def selection_boruta(X, y):
-    rf = RandomForestClassifier(n_estimators=100, random_state=42)
-    boruta_selector = BorutaPy(rf, n_estimators='auto', random_state=42)
-    boruta_selector.fit(X, y)
-    return boruta_selector.support_, boruta_selector.support_weak_
